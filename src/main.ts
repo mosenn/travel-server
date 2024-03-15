de1f4d4e52d269b2d00e263bb5fd32ac525c8cc9
unknown <mohseen_abasi@yahoo.com>
2024-03-15 11:59:49 +0330

    cors true

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,29 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule , { cors: true });
 
   app.enableCors({ origin: 'https://svtvapp.vercel.app', credentials: true });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    transform:true,
-    transformOptions:{
-      enableImplicitConversion:true,
-    }
-  }))
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3000/',
+      'https://svtvapp.vercel.app',
+      'https://svtvapp.vercel.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3020);
 }
 bootstrap();

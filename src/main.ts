e4850833528a1362bfe87d4e46cd78eda13c51c4
unknown <mohseen_abasi@yahoo.com>
2024-03-15 12:05:52 +0330

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , { cors: true });
 
-  app.enableCors({ origin: 'https://svtvapp.vercel.app', credentials: true });
+  // app.enableCors({ origin: 'https://svtvapp.vercel.app', credentials: true });
 
   app.enableCors({
     origin: [

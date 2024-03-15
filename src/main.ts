409940488b080f6c923821a1389737a398a564a2
unknown <mohseen_abasi@yahoo.com>
2024-03-15 11:53:53 +0330

    set address for cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-  app.enableCors({ origin: '*', credentials: true });
+  app.enableCors({ origin: 'https://svtvapp.vercel.app', credentials: true });
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true,
     transform:true,

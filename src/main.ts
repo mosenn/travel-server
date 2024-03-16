e4fc39bf270edf03d1cac0b2b392164ccda982e6
unknown <mohseen_abasi@yahoo.com>
2024-03-16 15:45:45 +0330

    factory cors true

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule ,{cors:true, });
 
   //* work on local
   // app.enableCors();
